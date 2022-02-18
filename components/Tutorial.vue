a672e7e2a2ad77e293a1c428ec99ed0a4a26dd26
winwinsanhaji <amine.sanhaji@win-win.com>
2022-02-18 14:56:23 +0100

    add globe page

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -26,15 +26,15 @@
         <div class="px-6 pt-4 pb-2">
           <span
             class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-            >#photography</span
+            >#3D</span
           >
           <span
             class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-            >#travel</span
+            >#Threejs</span
           >
           <span
             class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-            >#winter</span
+            >#Test</span
           >
         </div>
       </div>
@@ -59,15 +59,15 @@
         <div class="px-6 pt-4 pb-2">
           <span
             class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-            >#photography</span
+            >#3D</span
           >
           <span
             class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-            >#travel</span
+            >#Threejs</span
           >
           <span
             class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-            >#winter</span
+            >#Test</span
           >
         </div>
       </div>
@@ -92,15 +92,48 @@
         <div class="px-6 pt-4 pb-2">
           <span
             class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-            >#photography</span
+            >#3D</span
           >
           <span
             class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-            >#travel</span
+            >#Threejs</span
           >
           <span
             class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-            >#winter</span
+            >#Test</span
+          >
+        </div>
+      </div>
+      <!--Card 3-->
+      <div class="rounded overflow-hidden shadow-lg">
+        <a href="/globe">
+          <img
+            class="w-full"
+            src="@/assets/screenshots/globe.png"
+            alt="Mountain"
+        /></a>
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl text-black mb-2">
+            <a href="/globe">Globe</a>
+          </div>
+          <p class="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, Nonea! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span
+            class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+            >#3D</span
+          >
+          <span
+            class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+            >#Threejs</span
+          >
+          <span
+            class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+            >#Test</span
           >
         </div>
       </div>
@@ -125,15 +158,15 @@
         <div class="px-6 pt-4 pb-2">
           <span
             class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-            >#photography</span
+            >#3D</span
           >
           <span
             class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-            >#travel</span
+            >#Threejs</span
           >
           <span
             class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-            >#winter</span
+            >#Test</span
           >
         </div>
       </div>
@@ -155,15 +188,15 @@
         <div class="px-6 pt-4 pb-2">
           <span
             class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-            >#photography</span
+            >#3D</span
           >
           <span
             class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-            >#travel</span
+            >#Threejs</span
           >
           <span
             class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-            >#winter</span
+            >#Test</span
           >
         </div>
       </div>
@@ -188,15 +221,15 @@
         <div class="px-6 pt-4 pb-2">
           <span
             class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-            >#photography</span
+            >#3D</span
           >
           <span
             class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-            >#travel</span
+            >#Threejs</span
           >
           <span
             class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-            >#winter</span
+            >#Test</span
           >
         </div>
       </div>
---
+++ pages/globe/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <GlobeGeo />
+</template>
+
+<script>
+import GlobeGeo from "@/components/points/GlobeGeo";
+export default {
+  components: {
+    GlobeGeo,
+  },
+};
+</script>
